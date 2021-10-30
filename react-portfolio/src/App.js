4a45f83e8eb5248897b7f8ba804d2be52b192e50
Troy <troyat88@gmail.com>
2021-10-29 20:31:43 -0700

    setting up deploy

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -3,22 +3,119 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+<div>
+  <header>
+    <h1>Troy Atkinson</h1>
+    <h4>Future Full-Stack Developer</h4>
+  </header>
+  <nav>
+    <ul>
+      <li>
+        <a href="#about-me">About Me</a>
+      </li>
+      <li>
+        <a href="#projects">Projects</a>
+      </li>
+      <li>
+        <a href="#contact-info">Contact</a>
+      </li>
+      <li>
+        <a href="https://www.linkedin.com/in/troy-atkinson-uw/" target="_blank">LinkedIn</a>
+      </li>
+      <li>
+        <a href="./assets/images/CODING RESUME.docx" target="_blank">Resume</a>
+      </li>
+    </ul>
+  </nav>
+  <main>
+    <aside>
+      <h2 id="about-me">About Me</h2>
+      <img id="headshot" src="assets/images/headshot.jpg" alt="headshot of Troy Atkinson" />
+      <p className="headline"><b>BA Communication Studies</b></p>
+      <p className="headline">University of Washington</p>
+      <p className="headline">class of 2018</p>
+      <hr />
+      <p className="headline"><b>US Navy</b></p>
+      <p className="headline">2008-2014</p>
+    </aside>
+    <p className="bio">I am currently enrolled in the University of Washington's Coding Bootcamp. 
+      My plans are to find a career using my passion for design, creativity and problem solving as a developer.
+      I am drawn to web developement by a desire to better understand the ever changing digital world we find ourselves living in daily.</p>
+    <p className="bio"> Before joining Coding Bootcamp I was a staff manager and personal trainer at a gym in Seattle. before that, my studies at the Univerity of Washington focused on Journalism. 
+      And before college I had the privelage of travelling the seas and working with people from around the world while serving in the US Navy.  
+    </p>
+    <br />
+    <h4>Interests</h4>
+    <ul id="interests">
+      <li>UX/UI Design</li>
+      <li>App Developement</li>
+      <li>Product Management</li>
+    </ul>
+    <h4>Skills</h4>
+    <ul id="skills">
+      <li>Copy Writing</li>
+      <li>Spanish</li>
+      <li>Team Management</li>
+    </ul>
+  </main>
+  <article>
+    <aside>
+      <h2 id="projects">Projects</h2>
+      <p> I will be working on projects on my way to my coding certificate. Click on the project image to visit and try out my applications. </p>
+      <br /> 
+      <p><a href="https://github.com/troyat88" target="blank"><img src="assets/images/GitHub-Mark-64px.png" /></a></p>
+      <a href="https://github.com/troyat88">Visit my gitHub to see more!</a>
+    </aside>
+    <div className="project">
+      <h3>Find My Plate</h3>
+      <div className="grow">
+        <p><a href="https://troyat88.github.io/Find-My-Plate/" target="_blank">
+            <img className="hover" src="assets/images/Screenshot copy.png" alt="screenshot of Find My Plate application" /></a></p>
+        An easy to use application that allows users to find dining options in their current area or zip-code, search by cuisine, and search menus.
+      </div>
+    </div> 
+    <div className="project">
+      <h3>Your Forum</h3>
+      <div className="grow">
+        <p><a href="https://pacific-journey-91002.herokuapp.com/LoginRegister" target="_blank">
+            <img className="hover" src="assets/images/YourForum.png" alt="screenshot of Your Forum application" /></a></p>
+        A public discussion forum where users can create groups, topics, and email invites for participants.
+      </div>
     </div>
+    <div className="project">
+      <h3>Weather Dashboard</h3>
+      <div className="grow">
+        <p><a href="https://troyat88.github.io/Weather-Dashboard/" target="_blank">
+            <img className="hover" src="assets/images/Weather-Screenshot.png" alt="screenshot of Weather Dashboard application" /></a></p>
+        A dashboard where viewers can view weather conditions and a forecast by city.
+      </div>
+    </div>
+  </article>
+  <div id="contact">
+    <h2 id="contact-info">Contact Me</h2>
+    <div className="contactInfo">
+      <img src="assets/images/Location.jpg" alt="credit: Teewara Soontorn for TheNounProject.com" /> <p>Seattle, Washington</p>
+      <img src="assets/images/EMAIL.jpg" alt="credit: Icon Fair for TheNounProject.com" /> <p>troyat88@gmail.com</p>
+      <img src="assets/images/phone.jpg" alt="credit: Gregor Cresnor for TheNounProject.com" /> <p>956-525-9865</p>
+    </div>
+  </div>
+  <footer>
+    <p>Thanks for stopping by! </p>
+    <br />
+    <p>Icon Images courtesy of The Noun Project</p>
+  </footer>
+</div>
+
+
+
+
+
+   
+    
+
+
+    
+
   );
 }
 
